154373691cd7c2673d023e11fa02527d82ebb08d
powerm9 <powerm9@tcd.ie>
2025-11-12 16:13:46 +0000

    doesnt break if rosdeps already installed

--- install.sh
+++ install.sh
@@ -11,7 +11,18 @@ echo "Installing prerequisites..."
 sudo bash setup/get-prerequisites
 
 echo "Installing ROS2..."
-sudo bash setup/get-ros
+if ! sudo bash setup/get-ros; then
+    # Check if the specific error file exists
+    if [ -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+        echo "Error: default sources list file already exists. Removing and retrying..."
+        sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
+
+        echo "Retrying ROS2 installation..."
+        sudo bash setup/get-ros
+    fi
+else
+    echo "ROS2 installation completed successfully."
+fi
 
 echo "Installing Gazebo..."
 sudo bash setup/get-gazebo
@@ -40,26 +51,27 @@ else
     git submodule update --init --recursive
 fi
 
-echo "Installing Python requirements..."
+echo "\nInstalling Python requirements...\n"
 cd "FT-FSAI-23"
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 cd $DIR 
-echo "Building workspace using colcon..."
+echo "\nBuilding workspace using colcon...\n"
 colcon build --symlink-install || {
     echo "Build failed once, retrying..."
-    colcon build --symlink-install || {
-    	echo "Oops failed again, one more time..."
-		colcon build --symlink-install
-	}
+    colcon build --symlink-install 
 }
+
+echo "\nPost sucessful colcon build\n"
+colcon build --symlink-install
 
 echo "========================================="
 echo "FT Autonomous setup complete"
 echo "!!May need to run cbuildsym again!!"
 echo "Use ros2init to initialise... ros2"
-echo "  .w       # to enter workspace"
+echo "	.w	 # opens top dir and sources setup files"
+echo "  .r       # to source the setup files"
 echo "  ros2 ... # to run ROS2 commands"
 echo "========================================="
 
